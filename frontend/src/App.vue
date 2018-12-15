7616cfeebe549c2de0c74630993a86c3a55de9a2
Yi Zhang <yi.zhang7210@gmail.com>
2018-12-15 16:52:29 -0500

    [Auth] Add sign in by Google button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="gSignIn" class="g-signin2" data-onsuccess="onSignIn"></div>
   </div>
 </template>
 
@@ -12,7 +13,22 @@ export default {
   name: 'app',
   components: {
     HelloWorld
-  }
+  },
+  methods: {
+    onSignIn: (googleUser) => {
+      var profile = googleUser.getBasicProfile();
+      console.log('auth response: ' + googleUser.getAuthResponse().id_token);
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    },
+  },
+  mounted() {
+    gapi.signin2.render('gSignIn', {
+      onsuccess: this.onSignIn
+    })
+  },
 }
 </script>
 
