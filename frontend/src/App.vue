db4568c3dce86597bee5ac84e28c23e5593b8e9b
Yi Zhang <yi.zhang7210@gmail.com>
2018-12-15 18:02:30 -0500

    [Auth] Add sign out button

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Landing msg="Welcome to Syllable"/>
     <div id="gSignIn" class="g-signin2" data-onsuccess="onSignIn"></div>
+    <button class="g-signout" v-on:click="onSignOut">Sign out</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from './components/Landing.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Landing
   },
   methods: {
     onSignIn: (googleUser) => {
@@ -22,6 +22,12 @@ export default {
       console.log('Name: ' + profile.getName());
       console.log('Image URL: ' + profile.getImageUrl());
       console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    },
+    onSignOut: (googleUser) => {
+      var auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(function () {
+        console.log('User signed out.');
+      });
     },
   },
   mounted() {
@@ -35,10 +41,18 @@ export default {
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 10%;
+}
+
+.g-signout {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  margin-top: 20px;
+  border-width: 0;
+  width: 120px;
+  height: 40px;
 }
 </style>
