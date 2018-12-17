d696ca7f28b49f06790e35e6c941e22a2229c59d
Yi Zhang <yi.zhang7210@gmail.com>
2018-12-17 18:58:33 -0500

    [Auth] Get Google sign-in/sign-out consistent. Move to landing component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
-    <Landing msg="Welcome to Syllable"/>
-    <div id="gSignIn" class="g-signin2" data-onsuccess="onSignIn"></div>
-    <button class="g-signout" v-on:click="onSignOut">Sign out</button>
+    <Landing/>
   </div>
 </template>
 
@@ -13,46 +11,14 @@ export default {
   name: 'app',
   components: {
     Landing
-  },
-  methods: {
-    onSignIn: (googleUser) => {
-      var profile = googleUser.getBasicProfile();
-      console.log('auth response: ' + googleUser.getAuthResponse().id_token);
-      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-      console.log('Name: ' + profile.getName());
-      console.log('Image URL: ' + profile.getImageUrl());
-      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    },
-    onSignOut: (googleUser) => {
-      var auth2 = gapi.auth2.getAuthInstance();
-      auth2.signOut().then(function () {
-        console.log('User signed out.');
-      });
-    },
-  },
-  mounted() {
-    gapi.signin2.render('gSignIn', {
-      onsuccess: this.onSignIn
-    })
-  },
+  }
 }
 </script>
-
 <style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin-top: 10%;
-}
-
-.g-signout {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  font-size: 16px;
-  margin-top: 20px;
-  border-width: 0;
-  width: 120px;
-  height: 40px;
 }
 </style>
--- frontend/src/components/Landing.vue
+++ frontend/src/components/Landing.vue
@@ -1,21 +1,58 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
+  <div class="landing">
+    <h1>Welcome to Syllable</h1>
+    <div id="gSignIn" class="g-signin2" data-onsuccess="onSignIn"></div>
+    <button class="g-signout" v-on:click="onSignOut"> Sign out {{ this.userName }}</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Landing',
-  props: {
-    msg: String
+  data: () => ({
+      userName: ''
+  }),
+  mounted() {
+    gapi.signin2.render('gSignIn', {
+      onsuccess: this.onSignIn
+    })
+  },
+  methods: {
+    onSignIn(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      this.$forceUpdate();
+      console.log('auth response: ' + googleUser.getAuthResponse().id_token);
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      this.userName = '(' + profile.getName() + ')';
+      console.log(this.userName);
+
+    },
+    onSignOut(googleUser) {
+      var auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(() => {
+        this.userName = '';
+        this.$forceUpdate();
+      });
+    },
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.hello {
-  margin-bottom: 20px;
+.landing {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px;
+}
+.g-signout {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  margin-top: 20px;
+  border-width: 0;
+  width: 120px;
+  height: 40px;
 }
 </style>
