8b21157b2202a40f8138fb9e59b8dc72ebaa346e
Yi Zhang <yi.zhang7210@gmail.com>
2018-12-22 11:35:19 -0500

    [Setup] Linting

--- server/api/serializers/users.py
+++ server/api/serializers/users.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
-from api.models.users import User
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
     name = serializers.CharField(max_length=100)
     token = serializers.CharField()
+
+    def create(self, validated_data):
+        # Override abstract base class
+        pass
+
+    def update(self, instance, validated_data):
+        # Override abstract base class
+        pass
--- server/api/views/users.py
+++ server/api/views/users.py
@@ -1,16 +1,14 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView
+from rest_framework import status
 from api.serializers.users import LoginSerializer
 from api.services import users as user_service
-from rest_framework import status
 
 class UserAuthView(CreateAPIView):
 
     serializer_class = LoginSerializer
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         user_id = user_service.process_sign_in(
             request.data['name'],
             request.data['email'],
@@ -18,5 +16,5 @@ class UserAuthView(CreateAPIView):
         )
         if user_id:
             return Response("success")
-        else:
-            return Response("failed", status=status.HTTP_401_UNAUTHORIZED)
+
+        return Response("failed", status=status.HTTP_401_UNAUTHORIZED)
