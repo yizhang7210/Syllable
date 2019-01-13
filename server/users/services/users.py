e506d95d05546e39973b46fb72558df4f90fcc40
Yi Zhang <yi.zhang7210@gmail.com>
2019-01-13 15:15:47 -0500

    [Orgs] Orgs with no domains should only be accessible by creator

--- server/users/services/users.py
+++ server/users/services/users.py
@@ -33,12 +33,12 @@ def get_by_email(email):
     return users_dao.get_by_email(email)
 
 def update_org_info(user_email, user_domain):
-    if user_domain is None:
-        return
-
     current_user_org = get_current_org(user_email)
     if current_user_org is not None and current_user_org.domain != user_domain:
         remove_user_from_org(user_email, current_user_org.id)
+
+    if user_domain is None:
+        return
 
     org_with_domain = orgs_dao.get_by_domain(user_domain)
     if org_with_domain is not None and not is_in_org(user_email, org_with_domain.id):
