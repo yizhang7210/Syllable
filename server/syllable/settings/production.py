c0ecaf9f4affae2fd7c40c39fe4cb0b21650e364
Yi Zhang <yi.zhang7210@gmail.com>
2019-04-16 22:43:19 -0400

    [Setup] Add production config settings

--- server/syllable/settings/production.py
+++ server/syllable/settings/production.py
@@ -1,4 +1,19 @@
 from .base import *
+import os
 
-# Production configs
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+JWT_SECRET = os.environ['JWT_SECRET']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+    }
+}
+
+
+ALLOWED_HOSTS = [".us-east-2.elasticbeanstalk.com", "localhost"]
